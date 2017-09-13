a7c29a9806a0c7f1ef0a3c0658722031c8170187
Minato <nadir.berrara@gmail.com>
2017-09-13 13:35:07 +0200

    feature missing: remove transition

--- 01-Drumkit/script.js
+++ 01-Drumkit/script.js
@@ -1,3 +1,18 @@
-.key {
-    border: 2px solid black;
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // pas d'erreur si aucun audio associé
+  audio.currentTime = 0; // permet d'enchainer les touches
+  audio.play();
+  key.classList.add("playing"); //add a class to the key downed
 }
+
+// function removeTransition(e) {
+//   if (e.propertyName !== "transition") return;
+//   this.classList.remove("playing");
+// }
+
+// const keys = document.querySelectorAll(".key");
+// keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
