4bcfc0c90dc4f4a15ce7ffa8c629158b0decda8c
Minato <nadir.berrara@gmail.com>
2017-09-15 12:43:08 +0200

    01-add GIF

--- 01-Alien/script.js
+++ 01-Alien/script.js
@@ -1,4 +1,6 @@
 const keys = Array.from(document.querySelectorAll(".key"));
+const wave = document.querySelector(".wave2")
+
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
 function playSound(e) {
@@ -6,9 +8,16 @@ function playSound(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return; // pas d'erreur si aucun audio associé
   key.classList.add("playing"); //add a class to the key downed
+  wave.classList.remove("wave-visible");
+
+  setTimeout(function () {
+    wave.classList.add("wave-visible")
+  }, 2000);
+
   audio.currentTime = 0; // permet d'enchainer les touches
   audio.play();
 }
+
 
 function removeTransition(key) {
   if (key.propertyName !== "transform") return;
